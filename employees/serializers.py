5426bc30d851166fd1473b51469a14c176e98e86
roschel <joaomroschel@gmail.com>
2023-03-30 17:39:17 -0300

    urls and views

--- employees/serializers.py
+++ employees/serializers.py
@@ -5,4 +5,4 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ('id', 'name', 'email')
+        fields = ('id', 'name', 'email', 'department')
--- employees/views.py
+++ employees/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Employee
 from .serializers import EmployeeSerializer
+from departments.serializers import DepartmentSerializer
 
 
 # Create your views here.
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    department = DepartmentSerializer(read_only=True)
