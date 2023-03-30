ba77d905e773af173f2d09365476beec3af610e8
roschel <joaomroschel@gmail.com>
2023-03-30 17:04:22 -0300

    app departments

--- departments/admin.py
+++ departments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Department
 
 # Register your models here.
-admin.site.register(Employee)
+admin.site.register(Department)
