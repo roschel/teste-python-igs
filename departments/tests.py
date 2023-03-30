092589bfa08cb5d2b3943d664fc531dc790a2572
roschel <joaomroschel@gmail.com>
2023-03-30 20:48:07 -0300

    including a few tests

--- departments/tests.py
+++ departments/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from departments.models import Department
+
+
+class TestDepartments(APITestCase):
+    url = '/api/v1/departments'
+
+    def setUp(self):
+        Department.objects.create(name="department_test")
+
+    def test_get_departments(self):
+        response = self.client.get(f"{self.url}/", headers={"Content-Type": "application/json"})
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(result, list)
+        self.assertEqual(result[0]["name"], "department_test")
+
+    def test_get_department_by_id(self):
+        response = self.client.get(f"{self.url}/1/", headers={"Content-Type": "application/json"})
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(result, dict)
+        self.assertEqual(result["name"], "department_test")
--- employees/tests.py
+++ employees/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Employee
+from departments.models import Department
 
-# Create your tests here.
+
+class TestEmployees(APITestCase):
+    url = '/api/v1/employees'
+
+    def setUp(self):
+        department = Department.objects.create(name="department_test")
+        Employee.objects.create(name="John Doe", email="john@email.com", department=department)
+
+    def test_get_employees(self):
+        response = self.client.get(f"{self.url}/", headers={"Content-Type": "application/json"})
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(result, list)
+        self.assertEqual(result[0]["name"], "John Doe")
+
+    def test_get_employee_by_id(self):
+        response = self.client.get(f"{self.url}/1/", headers={"Content-Type": "application/json"})
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(result, dict)
+        self.assertEqual(result["name"], "John Doe")
